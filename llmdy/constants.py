5c09e340a824ee23953531a4397c3d01a6b9da23
Joseph Marbella <dieptuber@gmail.com>
2025-03-03 22:34:57 -0700

    :wrench: chore(config): Improve Redis URI handling

--- llmdy/constants.py
+++ llmdy/constants.py
@@ -1,8 +1,9 @@
 import os
 from typing import Literal
 import dotenv
+import redis
 
-dotenv.load_dotenv(os.getenv("ENV_PATH", ".env"))
+dotenv.load_dotenv(dotenv_path=os.getenv("ENV_PATH", ".env"), override=True)
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
@@ -31,7 +32,14 @@ CACHE_TTL = int(os.getenv("CACHE_TTL", "3600"))
 if CACHE_TTL < 0:
     raise ValueError("CACHE_TTL must be a non-negative integer")
 
-REDIS_URI = os.getenv("REDIS_URI")
+REDIS_URI = os.getenv("REDIS_URI", "redis://localhost:6379/0")
+
+if REDIS_URI != None:
+    response = redis.Redis.from_url(REDIS_URI).ping()
+    if response == False:
+        raise ValueError(
+            f"Failed to connect to redis instance. Is it running?\n\tURL: \"{REDIS_URI}\"")
+
 
 READERLM_PROMPT = """<|im_start|>Extract the main content from the given HTML and convert it to Markdown format.<|im_end|>
             <|im_start|>user
