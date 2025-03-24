4ec31d9ae75454f3f3644c1318d1e0306f6dc580
Joseph Marbella <josephmarbella@arizona.edu>
2025-03-24 01:26:34 -0700

    fix(tests): mocking `open` module returns `MagicMock` object when it should be a string

--- tests/agents/test_transcript2md.py
+++ tests/agents/test_transcript2md.py
@@ -1,18 +1,19 @@
-from typing import Callable
-from mock import MagicMock, mock_open, patch
-from llmdy.agents.transcript2md import Transcript2Markdown
-from llmdy.types import YTInfo
+from mock import MagicMock, patch
+import llmdy.agents.transcript2md
+from llmdy.llmdy_types import YTInfo
 
 
 @patch('llmdy.agents.transcript2md.agent.completions.create')
 @patch('builtins.open')
-def test_convert(_: MagicMock, mock_create: MagicMock):
+def test_convert(mock_open: MagicMock, mock_create: MagicMock):
     info: YTInfo = {
         "uploader": "uploader",
         "chapters": [],
         "description": "description",
         "fulltitle": "fulltitle"
     }
+
+    mock_open.return_value.__enter__.return_value.read.return_value = ""
 
     def c(content: str):
         completion = MagicMock()
@@ -30,7 +31,7 @@ def test_convert(_: MagicMock, mock_create: MagicMock):
     mock_create.return_value = [c(token) for token in tokenize(
         "<|begin_output|># Full Title\nHello World")]
 
-    transcript2md = Transcript2Markdown(
+    transcript2md = llmdy.agents.transcript2md.Transcript2Markdown(
         "hello world", info, file_name="file")
 
     assert str(transcript2md) == 'transcript2md/file'
